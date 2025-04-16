b56a933e2516da1450363a652c633dc3995ab0a1
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-16 15:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function insert_Row() {
     //Write your code here
   let mytable=document.getElementById("sampleTable");
-	var row=mytable.insertrow(0);
+	var row=mytable.insert_row(0);
 	var cell1=mytable.insertCell(0);
 	var cell2=mytable.insertCell(1);
   cell1.innerHTML="New Cell1";
 	cell2.innerHTML="New Cell2";
 }
+insert_Row.addEventListener("insert_Row"(e)=>){
+    insert_Row();	
+});
